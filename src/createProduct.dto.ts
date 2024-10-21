dd35da08035f0a5cfac7a2ad6493c37711c06043
MBotond21 <m.botond07@gmail.com>
2024-10-21 13:45:49 +0200

    hell nah

--- src/createProduct.dto.ts
+++ src/createProduct.dto.ts
@@ -1,4 +1,12 @@
+import { IsDefined, IsNumber, IsString, Min } from "class-validator";
+
 export class CreateProductDto{
+    @IsDefined()
+    @IsString()
     name: string;
+
+    @IsDefined()
+    @IsNumber()
+    @Min(1)
     price: number;
 }
