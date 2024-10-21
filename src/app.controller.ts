533b092bf1447bdbe6cc845e0611614b02a3289a
MBotond21 <m.botond07@gmail.com>
2024-10-21 13:30:08 +0200

    nah

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Param, Delete, Body, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateProductDto } from './createProduct.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
@@ -12,4 +13,40 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+
+  #products = [
+    {
+      name: "Bucket",
+      price: 3500
+    },
+    {
+      name: 'REST API for dummies',
+      price: 7850
+    },
+    {
+      name: 'Tablet',
+      price: 45000
+    }
+  ];
+
+  @Get('products')
+  getProducts() {
+    return this.#products;
+  }
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string) {
+    return this.#products[Number(id)];
+  }
+
+  @Delete('products/:id')
+  deleteProduct(@Param('id') id: string) {
+    this.#products.splice(Number(id), 1);
+  }
+
+  @Post('products')
+  newProduct(@Body() createProductDto: CreateProductDto){
+    this.#products.push(createProductDto);
+  }
+
 }
